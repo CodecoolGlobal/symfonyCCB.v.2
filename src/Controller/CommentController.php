2f86bf19301de83434b7180ea7d8d26955d57f1c
Adrian-P0gar <pogar.adi@gmail.com>
2020-09-24 00:49:02 +0300

    add comment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -6,6 +6,8 @@ use App\Entity\Comment;
 use App\Entity\Post;
 use App\Entity\UserProfile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
@@ -15,6 +17,7 @@ class CommentController extends AbstractController
      */
     public function index($targetId, $postId)
     {
+
         $entityManager = $this->getDoctrine()->getManager();
         $creatorProfile = $entityManager->getRepository(UserProfile::class)->find($targetId);
         $creatorLastName = $creatorProfile->getLastName();
@@ -23,11 +26,37 @@ class CommentController extends AbstractController
 
         $post = $this->getDoctrine()->getRepository(Post::class)->findOneBy(['id' => $postId]);
 
-
         return $this->render('comment/index.html.twig', [
             'controller_name' => 'CommentController', 'id'=> $postId, 'comments' => $comments, 'creatorFirstName'=> $creatorFirstName,
-            'creatorLastName' => $creatorLastName, 'postMessage' => $post->getMessage(),
+            'creatorLastName' => $creatorLastName, 'postMessage' => $post->getMessage(), 'targetId' => $targetId, 'wallId' => $post->getTargetProfileId(),
         ]);
+    }
+
+    /**
+     * @Route("/doComment/{targetId}/{postId}", name="doComment", methods="POST")
+     */
+    public function doComment($targetId, $postId,Request $request) : Response
+    {
+        $userDetail = $this->getUser()->getId();
+        $creatorProf= $this->getDoctrine()->getRepository(UserProfile::class)->findOneBy(['user_id' => $userDetail]);
+
+        $message = $request->request->get('message');
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $comment = new Comment();
+
+        $comment->setUserProfileId($creatorProf->getId());
+        $comment->setPostId($postId);
+        $comment->setMessage($message);
+        $comment->setImageId(6);
+        $comment->setDeleted(0);
+
+        $entityManager->persist($comment);
+        $entityManager->flush();
+
+        return $this->redirect("/comment/".$targetId."/".$postId, 301);
+
     }
 
     public function getAllComments($postId)
@@ -45,11 +74,12 @@ class CommentController extends AbstractController
 
             $tempList += ['firstName' => $getCreatorInfo->getFirstName()];
             $tempList += ['lastName' => $getCreatorInfo->getLastName()];
-
+            $tempList += ['creatorId' => $getCreatorInfo->getId()];
             $finalPostsList += [$counter => $tempList];
             $counter++;
 
         }
+
      return $finalPostsList;
     }
 }
