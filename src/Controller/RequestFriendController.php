ce2a50e686a6ba7a0432020aef00be172dba7386
Adrian-P0gar <pogar.adi@gmail.com>
2020-09-24 14:07:03 +0300

    friend request  page

--- src/Controller/RequestFriendController.php
+++ src/Controller/RequestFriendController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\FriendsList;
 use App\Entity\User;
+use App\Entity\UserProfile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +15,37 @@ class RequestFriendController extends AbstractController
      */
     public function index()
     {
-        $user = $this->getUser();
+        $user = $this->getUser()->getId();
+        $userId = ($this->getDoctrine()
+            ->getRepository(UserProfile::class)->findOneBy(['user_id' => $user ]))->getId();
 
+        $allFriendsRequest = $this->getAllFriends($userId);
 
         return $this->render('request_friend/index.html.twig', [
-            'controller_name' => 'RequestFriendController',
+            'controller_name' => 'RequestFriendController', 'allRequest'=> $allFriendsRequest,
         ]);
+    }
+
+    public function getAllFriends($wallId)
+    {
+        $friendsIdBySenderId = $this->getDoctrine()->getRepository(FriendsList::class)->selectAllFriendsBySenderId($wallId, 2);
+        $friendsIdByReceiverId = $this->getDoctrine()->getRepository(FriendsList::class)->selectAllFriendsByReceiverId($wallId, 2);
+        $allFriendsId = array_merge($friendsIdBySenderId, $friendsIdByReceiverId);
+        $allFriendsProfile = [];
+        foreach (array_merge($allFriendsId) as $arr) {
+            {
+
+                foreach ($arr as $id) {
+
+                    $bla = $this->getDoctrine()
+                        ->getRepository(UserProfile::class)
+                        ->findOneBy(['id' => $id]);
+                    array_push($allFriendsProfile, $bla);
+
+                }
+            }
+        }
+
+        return $allFriendsProfile;
     }
 }
